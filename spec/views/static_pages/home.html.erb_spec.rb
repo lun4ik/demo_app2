e709111fd8a58e9a8ffbcdc9b00bbdf31a121714
lun4ik <lun.sl.4ik@gmail.com>
2016-02-05 09:35:37 +0600

    Static pages compleate

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
-describe "static_pages/home.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
 end
